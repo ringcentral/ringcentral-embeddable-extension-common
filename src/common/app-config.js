624c22ead4b0e8d61e6eb1f5150c0d9f3821b60f
ZHAO Xudong <zxdong@gmail.com>
2019-04-01 08:29:44 +0800

    Export app version

--- src/common/app-config.js
+++ src/common/app-config.js
@@ -2,3 +2,4 @@
 
 export const thirdPartyConfigs = process.env.thirdPartyConfigs
 export const ringCentralConfigs = process.env.ringCentralConfigs
+export const appVersion = process.env.version || ''
